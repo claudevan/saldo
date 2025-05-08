9ff5551b0d754276c14c460c035764bc164534ae
Claudevan <claudevan.ti@gmail.com>
2025-05-08 20:31:32 -0300

    init: first commit

---
+++ src/main/java/github/com/claudevan/saldo/SaldoApplication.java
@@ -0,0 +1,13 @@
+package github.com.claudevan.saldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaldoApplication.class, args);
+	}
+
+}
